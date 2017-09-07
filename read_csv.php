4926d882f957bb12184cd7a04b8bade1da6c9d3d
sampalve3 <sampalve3@gmail.com>
2017-09-07 20:39:38 +0530

    reading factors from csv

--- read_csv.php
+++ read_csv.php
@@ -8,12 +8,15 @@
 $arr_influencers = array_map('str_getcsv', file('/var/www/html/research/bubblechart/bubble_chart_second.csv'));
 // print_r($arr_influencers);
 // array_shift($arr_influencers);
-array_shift($arr_influencers);
+$factors=array_shift($arr_influencers);
+$factor=array_shift($factors);
+// print_r($factors);
 
 
-$arrayNames=array("Sexual harassment","Violence","Abortion","Marriage Laws & Acts ( Divorce & Dowry)","Women -Empowerment","Sexual harassment at work","Abuse of Law","Maternal Health","Menstruation","School Education","entrepreneurship","Sanitation","Sex Work","Girl Child Victim (chilld Marriage + Child Trafficking)","Property Rights","Higher Education","Diseases","Moral Policing","Surrogacy / Adoption","Family Planning","Women in Agriculture","Vocational Education","Gender Inequality at work","Perks & Benefits","Malnutrition","Mortality Rate","Female Infanticide / Foeticide","Sex Education");
 
-$cluster_color = array("#e6550d","#6baed6","#31a354","#D900F7","#fdd0a2","#fd8d3c","#6baed6","#FF5733","#6baed6","#cfec84","#756bb1");
+// $arrayNames=array("Sexual harassment","Violence","Abortion","Marriage Laws & Acts ( Divorce & Dowry)","Women -Empowerment","Sexual harassment at work","Abuse of Law","Maternal Health","Menstruation","School Education","entrepreneurship","Sanitation","Sex Work","Girl Child Victim (chilld Marriage + Child Trafficking)","Property Rights","Higher Education","Diseases","Moral Policing","Surrogacy / Adoption","Family Planning","Women in Agriculture","Vocational Education","Gender Inequality at work","Perks & Benefits","Malnutrition","Mortality Rate","Female Infanticide / Foeticide","Sex Education");
+
+$cluster_color = array("#e6550d","#6baed6","#31a354","#D900F7","#fdd0a2","#fd8d3c","#6baed6","#FF5733","#6baed6","#cfec84","#756bb1","#F3FF00","#22249B","#02FFFB","#01FFF3");
 // 
 // foreach  ($arr_influencers as $key => $val) {
 foreach  ($arr_influencers as $single_influencer) {
@@ -29,7 +32,7 @@ foreach  ($arr_influencers as $single_influencer) {
 	$idx_cluster = key($single_influencer);
 	// echo "\r\n\r\n".$influencer.' '.$idx_cluster.' '.$tweets_cluster."\r\n\r\n";
 
-	$output[$arrayNames[$idx_cluster]][]=array(
+	$output[$factors[$idx_cluster]][]=array(
 		"name"=>$influencer,
 		"size"=>$tweets_cluster,
 		"color"=>$cluster_color[$idx_cluster]
@@ -58,7 +61,22 @@ foreach ($output as $key => $value) {
 		"children"=>$last_output
 		);
 
-	echo json_encode($jsonDone);
-	exit;
+	$json=json_encode($jsonDone);
+	$json1=substr($json, 1, -1);
+	// print_r($json1);
+
+	$fp = fopen('/var/www/html/research/charts/bubblechart/test.json', 'w');
+	// fwrite($fp, json_encode($jsonDone));
+	fwrite($fp, $json1);
+	// fclose($fp);exit;
+
+
+	// ftruncate($fh, $stat['size']-1);
+	// // ftruncate($fh, $stat['size']-2);
+	// // ftruncate($fh, 0);
+	// print_r($stat);
+
+	// rewind($fh);
+	// exit;
  
 ?>
